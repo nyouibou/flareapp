787da7e85b3ad07a0b8535edbd4aa14c9c5de134
Akshay TS <57423537+nyouibou@users.noreply.github.com>
2024-04-19 10:02:14 +0530

    screen update

--- lib/view/first_screen/firstscreen.dart
+++ lib/view/first_screen/firstscreen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flareapp/view/camerascreen/arcamerascreen.dart';
-import 'package:flareapp/view/camerascreen/camerascreen.dart';
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatelessWidget {
